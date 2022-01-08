07b59c2d3fe66182a8f6580f7787cfcb959078e3
Bahtiyorjon Arslonbekov <bahtiyorjonq777@gmail.com>
2022-01-08 22:56:26 +0100

    first commit

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,21 +1,21 @@
 // useState: greeting
 // http://localhost:3000/isolated/exercise/01.js
 
-import * as React from 'react'
+import React, {useState} from 'react'
 
 function Greeting() {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  const [name, setName] = useState('')
 
   function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={e => handleChange(e)} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
